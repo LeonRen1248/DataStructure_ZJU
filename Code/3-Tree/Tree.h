ff5d57eda1a9623561bf65b549dc93124a57567c
LeonRen1248 <yuren1248@163.com>
2021-10-14 17:39:07 +0800

    又写完了一部分

--- Code/3-Tree/Tree.h
+++ Code/3-Tree/Tree.h
@@ -16,15 +16,17 @@ public:
         element_type item;
         struct TreeNode *left;
         struct TreeNode *right;
-        TreeNode(element_type _item) : item(_item), left(nullptr), right(nullptr) { }
+        explicit TreeNode(element_type _item) : item(_item), left(nullptr), right(nullptr) { }
         TreeNode() : TreeNode(0) { }
     }TreeNode;
 
     Tree() : tree(nullptr) { }
     Tree(const char **arr, int n);
-    Tree(std::vector<const char *> vec);
+    explicit Tree(std::vector<const char *> vec);
     Tree(const Tree &tree);
     ~Tree();
+
+    Tree & operator=(const Tree &t);
 
     void pre_order_traversal(void (*fp)(element_type item)) const;
     void in_order_traversal(void (*fp)(element_type item)) const;
@@ -36,6 +38,8 @@ public:
 private:
     void oops(const char *str) const { std::cout << str << std::endl; }
     void delete_tree(TreeNode *tn);
+    TreeNode * copy_tree(TreeNode *tn);
+    
     void pre_order_traversal(TreeNode *tn, void (*fp)(element_type item)) const;
     void in_order_traversal(TreeNode *tn, void (*fp)(element_type item)) const;
     void post_order_traversal(TreeNode *tn, void (*fp)(element_type item)) const;
@@ -72,12 +76,58 @@ inline Tree::Tree(const char **arr, int n) : tree(nullptr) {
             node_queue.push(curr_node->right);
         }
     }
-    if (n % 2) {
+    if (!(n % 2)) {
         TreeNode *curr_node = node_queue.front();
         if (strcmp(arr[n - 1], "#")) {
             curr_node->left = new TreeNode(atoi(arr[n - 1]));
         }
     }
+}
+
+inline Tree::Tree(std::vector<const char *> vec) {
+    if (vec.size() <= 0) {
+        oops("Index out of boundary.");
+        return;
+    }
+    if (!strcmp(vec.at(0), "#")) {
+        return;
+    }
+    tree = new TreeNode(atoi(vec.at(0)));
+    std::queue<TreeNode *> node_queue;
+    node_queue.push(tree);
+    for (int i = 1; i <= (vec.size() - 1) / 2; ++i) {
+        TreeNode *curr_node = node_queue.front();
+        node_queue.pop();
+        const char *tmp = vec.at(i * 2 - 1);
+        if (strcmp(vec.at(i * 2 - 1), "#")) {
+            curr_node->left = new TreeNode(atoi(vec.at(i * 2 - 1)));
+            node_queue.push(curr_node->left);
+        }
+        if (strcmp(vec.at(i * 2), "#")) {
+            curr_node->right = new TreeNode(atoi(vec.at(i * 2)));
+            node_queue.push(curr_node->right);
+        }
+    }
+    if (!(vec.size() % 2)) {
+        TreeNode *curr_node = node_queue.front();
+        if (strcmp(vec.at(vec.size() - 1), "#")) {
+            curr_node->left = new TreeNode(atoi(vec.at(vec.size() - 1)));
+        }
+    }
+}
+
+inline Tree::TreeNode *Tree::copy_tree(TreeNode *tn) {
+    if (!tn) return nullptr;
+    TreeNode *tree_left = copy_tree(tn->left);
+    TreeNode *tree_right = copy_tree(tn->right);
+    TreeNode *root = new TreeNode(tn->item);
+    root->left = tree_left;
+    root->right = tree_right;
+    return root;
+}
+
+inline Tree::Tree(const Tree &t) {
+    tree = copy_tree(t.tree);
 }
 
 inline void Tree::pre_order_traversal(void (*fp)(element_type item)) const {
@@ -91,8 +141,26 @@ inline void Tree::pre_order_traversal(TreeNode *tn, void (*fp)(element_type item
     pre_order_traversal(tn->right, fp);
 }
 
-inline Tree::~Tree() {
-    delete_tree(tree);
+inline void Tree::in_order_traversal(void (*fp)(element_type item)) const {
+    in_order_traversal(tree, fp);
+}
+
+inline void Tree::in_order_traversal(TreeNode *tn, void (*fp)(element_type item)) const {
+    if (!tn) { return; }
+    in_order_traversal(tn->left, fp);
+    fp(tn->item);
+    in_order_traversal(tn->right, fp);
+}
+
+inline void Tree::post_order_traversal(void (*fp)(element_type item)) const {
+    post_order_traversal(tree, fp);
+}
+
+inline void Tree::post_order_traversal(TreeNode *tn, void (*fp)(element_type item)) const {
+    if (!tn) { return; }
+    post_order_traversal(tn->left, fp);
+    post_order_traversal(tn->right, fp);
+    fp(tn->item);
 }
 
 inline void Tree::delete_tree(TreeNode *tn) {
@@ -100,6 +168,17 @@ inline void Tree::delete_tree(TreeNode *tn) {
     delete_tree(tn->left);
     delete_tree(tn->right);
     delete tn;
+}
+
+inline Tree::~Tree() {
+    delete_tree(tree);
+}
+
+inline Tree & Tree::operator=(const Tree &t) {
+    delete_tree(tree);
+    tree = nullptr;
+    tree = copy_tree(t.tree);
+    return *this;
 }
 
 void print_value(Tree::element_type value) {
--- Code/3-Tree/TreeTest.cpp
+++ Code/3-Tree/TreeTest.cpp
@@ -1,17 +1,35 @@
 #include "Tree.h"
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
 void print_node(Tree::element_type value) {
-    cout << value;
+    cout << value << ", ";
 }
 
 int main(int argc, char const *argv[]) {
-    const char *arr[10] = {"1", "2", "3", "4", "5", "6", "7", "#", "8", "9"};
+    const char *arr[10] = {"1", "2", "3", "4", "5", "6", "7", "#", "8", "9", };
     Tree tree(arr, 10);
-    cout << tree << endl;
+    cout << tree << endl; // [ 1, 2, 4, 8, 5, 9, 3, 6, 7, ]
+
+    vector<const char *> vec{"5", "6", "7", "#", "8", "9", "1", "2", "3", "4", };
+    Tree tree2(vec);
+    cout << tree2 << endl; // [ 5, 6, 8, 2, 3, 7, 9, 4, 1, ]
+
+    Tree tree4 = tree2; // 此处是调用构造函数 Tree(const Tree &tree);
+    cout << tree4 << endl; // [ 5, 6, 8, 2, 3, 7, 9, 4, 1, ]
+
+    Tree tree3(tree2);
+    cout << tree3 << endl; // [ 5, 6, 8, 2, 3, 7, 9, 4, 1, ]
+
+    tree3 = tree;
+    cout << tree3 << endl; // [ 1, 2, 4, 8, 5, 9, 3, 6, 7, ]
+
+    tree3.pre_order_traversal(print_node); cout << endl;
+    tree3.in_order_traversal(print_node); cout << endl;
+    tree3.post_order_traversal(print_node); cout << endl;
     
     return 0;
 }
